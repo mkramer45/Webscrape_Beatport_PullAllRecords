3312bc2f1848ca32dcb6a222bfdea64dc228e3d0
Gabriel <g051x7db@googlemail.com>
2017-11-01 01:07:30 +0100

    added artist, price and label

--- 1026_BeatportCLI_Success_PullAllRecordsxxxxxxxxxxx.py
+++ 1026_BeatportCLI_Success_PullAllRecordsxxxxxxxxxxx.py
@@ -19,13 +19,15 @@ containers = page_soup.findAll("li",{"class":"bucket-item ec-item track"})
 
 conn = sqlite3.connect('Beatscrape.db')
 cursor = conn.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS BeatPortTechHouse(SongName TEXT)')
+cursor.execute('CREATE TABLE IF NOT EXISTS BeatPortTechHouse(SongName TEXT, SongArtist TEXT, SongPrice DECIMAL, SongLabel TEXT)')
 
 # MSK, artist name
 for container in containers:
-	song_Name = container["data-ec-name"]
-	cursor.execute("INSERT INTO BeatPortTechHouse VALUES (?)", (song_Name,))
-
+	song_name = container["data-ec-name"]
+	song_artist = container["data-ec-d1"]
+	song_price = container["data-ec-price"]
+	song_label = container["data-ec-brand"]
+	cursor.execute("INSERT INTO BeatPortTechHouse VALUES (?,?,?,?)", (song_name, song_artist, song_price, song_label))
 conn.commit()
 cursor.close()
 conn.close()
